e895bc0bdd1813b8ba201bd71f7ce81e481ec7ba
Akshay S <akshay@webllisto.com>
2019-11-05 16:54:48 +0530

    programs added

---
+++ add_two_numbers.py
@@ -0,0 +1,12 @@
+num1 = 30
+num2 = 40
+
+
+# Add two Numbers 
+sum = num1 + num2
+
+# First Trick
+print("Sum of Two Number: " + str(sum))
+
+# Second Trick
+print("Sum of {0} and {1} is {2}.".format(num1, num2, sum))
